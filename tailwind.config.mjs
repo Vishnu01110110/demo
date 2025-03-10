7431806a72abd16b0edf3768269968b3c8334a35
Vishnu01110110 <115927321+Vishnu01110110@users.noreply.github.com>
2025-03-09 20:39:06 -0400

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+// tailwind.config.mjs
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'simbie-purple': '#7B5CF0',
+        },
+        fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+        }
+      },
+    },
+    plugins: [],
+  }
